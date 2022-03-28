b340204ee62492c9fa52e709f5e50a814a330109
Collin Fogerty <clf9@njit.edu>
2022-03-27 21:29:25 -0400

    New pages added for proj2--4

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -10,11 +10,6 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
-    assert b'<a class="nav-link" href="/page5">OOP</a>' in response.data
-    assert b'<a class="nav-link" href="/page6">AAA Testing</a>' in response.data
-    assert b'<a class="nav-link" href="/page7">Calculator (OOP)</a>' in response.data
-    assert b'<a class="nav-link" href="/page8">Calculator (SOLID)</a>' in response.data
-
 
 def test_request_index(client):
     """This makes the index page"""
@@ -58,35 +53,8 @@ def test_request_page4(client):
     assert b"CI/CD" in response.data
 
 
-def test_request_page5(client):
-    """This makes the index page"""
-    response = client.get("/page5")
-    assert response.status_code == 200
-    assert b"OOP" in response.data
-
-
-def test_request_page6(client):
-    """This makes the index page"""
-    response = client.get("/page6")
-    assert response.status_code == 200
-    assert b"Arrange-Act-Assert" in response.data
-
-
-def test_request_page7(client):
-    """This makes the index page"""
-    response = client.get("/page7")
-    assert response.status_code == 200
-    assert b"Calculator" in response.data
-
-
-def test_request_page8(client):
-    """This makes the index page"""
-    response = client.get("/page8")
-    assert response.status_code == 200
-    assert b"Calculator" in response.data
-
 
 def test_request_page_not_found(client):
     """This makes the index page"""
-    response = client.get("/page9")
+    response = client.get("/page5")
     assert response.status_code == 404
