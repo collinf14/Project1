63d7b64b6b78d023be8b3eda92f89b60bdc51ff8
Collin Fogerty <clf9@njit.edu>
2022-03-27 20:31:51 -0400

    New pages added for proj2

--- app/context_processors/__init__.py
+++ app/context_processors/__init__.py
@@ -2,7 +2,7 @@ from os import getenv
 import datetime
 
 def utility_text_processors():
-    message = "hello world"
+    message = "Thank you for reading. H"
 
     def deployment_environment():
         return getenv('FLASK_ENV', None)
--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -1,5 +1,6 @@
 """This test the homepage"""
 
+
 def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
@@ -9,6 +10,11 @@ def test_request_main_menu_links(client):
     assert b'<a class="nav-link" href="/page2">Docker</a>' in response.data
     assert b'<a class="nav-link" href="/page3">Python/Flask</a>' in response.data
     assert b'<a class="nav-link" href="/page4">CI/CD</a>' in response.data
+    assert b'<a class="nav-link" href="/page5">OOP</a>' in response.data
+    assert b'<a class="nav-link" href="/page6">AAA Testing</a>' in response.data
+    assert b'<a class="nav-link" href="/page7">Calculator (OOP)</a>' in response.data
+    assert b'<a class="nav-link" href="/page8">Calculator (SOLID)</a>' in response.data
+
 
 def test_request_index(client):
     """This makes the index page"""
@@ -16,11 +22,13 @@ def test_request_index(client):
     assert response.status_code == 200
     assert b"IS601" in response.data
 
+
 def test_request_about(client):
     """This makes the index page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About Page" in response.data
+
 
 def test_request_page1(client):
     """This makes the index page"""
@@ -28,11 +36,13 @@ def test_request_page1(client):
     assert response.status_code == 200
     assert b"Git" in response.data
 
+
 def test_request_page2(client):
     """This makes the index page"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Docker" in response.data
+
 
 def test_request_page3(client):
     """This makes the index page"""
@@ -40,13 +50,43 @@ def test_request_page3(client):
     assert response.status_code == 200
     assert b"Python" in response.data
 
+
 def test_request_page4(client):
     """This makes the index page"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"CI/CD" in response.data
 
-def test_request_page_not_found(client):
+
+def test_request_page5(client):
     """This makes the index page"""
     response = client.get("/page5")
+    assert response.status_code == 200
+    assert b"OOP" in response.data
+
+
+def test_request_page6(client):
+    """This makes the index page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"Arrange-Act-Assert" in response.data
+
+
+def test_request_page7(client):
+    """This makes the index page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"Calculator" in response.data
+
+
+def test_request_page8(client):
+    """This makes the index page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"Calculator" in response.data
+
+
+def test_request_page_not_found(client):
+    """This makes the index page"""
+    response = client.get("/page9")
     assert response.status_code == 404
