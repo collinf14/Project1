554d351f0c406bf38a2eecdd907fac716ecaedfc
Collin Fogerty <clf9@njit.edu>
2022-03-28 23:35:05 -0400

    proj2 finished

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -19,38 +19,66 @@ def test_request_index(client):
 
 
 def test_request_about(client):
-    """This makes the index page"""
+    """This makes the about page"""
     response = client.get("/about")
     assert response.status_code == 200
     assert b"About Page" in response.data
 
 
 def test_request_page1(client):
-    """This makes the index page"""
+    """This makes the GIT page"""
     response = client.get("/page1")
     assert response.status_code == 200
     assert b"Git" in response.data
 
 
 def test_request_page2(client):
-    """This makes the index page"""
+    """This makes the Docker page"""
     response = client.get("/page2")
     assert response.status_code == 200
     assert b"Docker" in response.data
 
 
 def test_request_page3(client):
-    """This makes the index page"""
+    """This makes the Python page"""
     response = client.get("/page3")
     assert response.status_code == 200
     assert b"Python" in response.data
 
 
 def test_request_page4(client):
-    """This makes the index page"""
+    """This makes the CICD page"""
     response = client.get("/page4")
     assert response.status_code == 200
     assert b"CI/CD" in response.data
+
+
+def test_request_page5(client):
+    """This makes the OOP page"""
+    response = client.get("/page5")
+    assert response.status_code == 200
+    assert b"OOP" in response.data
+
+
+def test_request_page6(client):
+    """This makes the AAA Testing page"""
+    response = client.get("/page6")
+    assert response.status_code == 200
+    assert b"AAA Testing" in response.data
+
+
+def test_request_page7(client):
+    """This makes the OOP Principles page"""
+    response = client.get("/page7")
+    assert response.status_code == 200
+    assert b"OOP" in response.data
+
+
+def test_request_page8(client):
+    """This makes the SOLID page"""
+    response = client.get("/page8")
+    assert response.status_code == 200
+    assert b"SOLID" in response.data
 
 
 
